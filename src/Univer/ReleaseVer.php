848bbe9f72e1f04947e4923caf6504fef6aed542
mr-older <ez@ez.tom.ru>
2023-11-12 17:08:48 +0600

    Changed algorithm finding release version by commit hash

--- src/Univer/ReleaseVer.php
+++ src/Univer/ReleaseVer.php
@@ -3,11 +3,15 @@
 namespace Univer;
 
 #
-# Detection last release version of given repo
-# after git commit save commit hash with command "git rev-parse HEAD >last_commit.sha"
-# to file into root directory (where LICENSE is located).
-# After pushing it goes to repo and can be read by this class.
-# Comparing that hash and hash of release gives the release version of your product
+# Algo
+#
+# Detection last release version of given repo.
+# Befor "git commit" user must save commit hash with command
+# "git rev-parse HEAD >last_commit.sha" to file in the project`s root directory (where LICENSE is located).
+# After pushing it goes to repo and can be read by this class. It became previous (!) SHA hash (before commit).
+#
+# Finding out child of that hash in commits, comparing child`s SHA and hash of release
+# gives us the release version of your product.
 #
 
 class releaseVer
@@ -20,7 +24,7 @@ class releaseVer
 	}
 
     /**
-	* @param  string $repo_name, ex. "mr-older/univer"
+	* @param  string $repo_name, i.e. "mr-older/univer"
 	*/
 	public function getVersion($repo_name) {
 		$this->error = "";
@@ -40,11 +44,34 @@ class releaseVer
 			return false;
 		}
 
-		curl_setopt($ch, CURLOPT_URL, ($url = $this->github_url.$repo_name."/tags"));
+		curl_setopt($ch, CURLOPT_URL, ($url = $this->github_url.$repo_name."/commits"));
 		curl_setopt($ch, CURLOPT_HEADER, 0);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-		curl_setopt($ch, CURLOPT_USERAGENT, "mr-older/univer");
+		curl_setopt($ch, CURLOPT_USERAGENT, "mr-older/univer");	// github req
+
+		if(empty($commits = curl_exec($ch))) {
+			$this->error = "Couldn`t get commits @ $url";
+			return false;
+		}
+
+		$commits = json_decode($commits, true);
+		$last_commit_hash = trim($last_commit_hash);
+
+		foreach((array) $commits as $commit) {
+			if($commit['parents']['sha'] != $last_commit_hash) {
+				continue;
+			}
+			$release_commit_hash = $commit['sha'];
+			break;
+		}
+
+		if(empty($release_commit_hash)) {
+			$this->error = "Couldn`t find commit for $last_commit_hash";
+			return false;
+		}
+
+		curl_setopt($ch, CURLOPT_URL, ($url = $this->github_url.$repo_name."/releases"));
 
 		if(empty($releases = curl_exec($ch))) {
 			$this->error = "Couldn`t get releases @ $url";
@@ -52,13 +79,13 @@ class releaseVer
 		}
 
 		$releases = json_decode($releases, true);
-		$last_commit_hash = trim($last_commit_hash);
 
 		foreach((array) $releases as $release) {
 			if($release['commit']['sha'] != $last_commit_hash) continue;
-			return $release['name'];	// zipball_url node_id commit
+			return $release['name'];
 		}
 
+		$this->error = "No release for commit $release_commit_hash";
 		return false;
 	}
 }
